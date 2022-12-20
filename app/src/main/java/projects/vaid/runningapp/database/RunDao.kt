50cbee75476be7471b1637f2b3536dba2fe4e552
Vaid Sjothonov <mode_private@mail.ru>
2022-12-20 13:39:56 +0300

    Add database classes

---
+++ app/src/main/java/projects/vaid/runningapp/database/RunDao.kt
@@ -0,0 +1,41 @@
+package projects.vaid.runningapp.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface RunDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRun(run: Run)
+
+    @Delete
+    suspend fun deleteRun(run: Run)
+
+    @Query("SELECT * FROM running_table ORDER BY timestamp DESC")
+    fun getAllRunsSortedByDate(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY timeInMillis DESC")
+    fun getAllRunsSortedByTime(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC")
+    fun getAllRunsSortedByDistance(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY averageSpeed DESC")
+    fun getAllRunsSortedByAvgSpeed(): LiveData<List<Run>>
+
+    /* functions for statistic */
+
+    @Query("SELECT SUM(timeInMillis) FROM running_table")
+    fun getTotalTimeInMillis(): LiveData<Long>
+
+    @Query("SELECT SUM(caloriesBurned) FROM running_table")
+    fun getTotalCaloriesBurned(): LiveData<Int>
+
+    @Query("SELECT SUM(distanceInMeters) FROM running_table")
+    fun getTotalTimeInDistance(): LiveData<Int>
+
+    @Query("SELECT AVG(averageSpeed) FROM running_table")
+    fun getTotalAvgSpeed(): LiveData<Float>
+
+}
