93a0764ee67a8fc3ef3eb3ba2f0ec7a2bd79c234
Vaid Sjothonov <mode_private@mail.ru>
2022-12-23 15:43:01 +0300

    Add navigation graph Set up navigation between fragments

--- app/src/main/java/projects/vaid/runningapp/ui/MainActivity.kt
+++ app/src/main/java/projects/vaid/runningapp/ui/MainActivity.kt
@@ -2,7 +2,11 @@ package projects.vaid.runningapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import projects.vaid.runningapp.R
 import projects.vaid.runningapp.database.RunDao
 import javax.inject.Inject
@@ -13,5 +17,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(nav_host_fragment_container.findNavController())
+
+        //показывать bottomNavigation только в выбранных фаргментах
+        nav_host_fragment_container.findNavController()
+            .addOnDestinationChangedListener{ _, destination, _ ->
+                when(destination.id){
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
+
     }
 }
--- app/src/main/java/projects/vaid/runningapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/projects/vaid/runningapp/ui/fragments/RunFragment.kt
@@ -1,8 +1,13 @@
 package projects.vaid.runningapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
+import kotlinx.android.synthetic.main.fragment_setup.*
 import projects.vaid.runningapp.R
 import projects.vaid.runningapp.ui.viewmodels.MainViewModel
 
@@ -10,4 +15,12 @@ import projects.vaid.runningapp.ui.viewmodels.MainViewModel
 class RunFragment :  Fragment(R.layout.fragment_run){
 
     private val viewModel : MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fab.setOnClickListener{
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 }
